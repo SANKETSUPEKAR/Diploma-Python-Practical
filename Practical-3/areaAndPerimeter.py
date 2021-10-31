ffa69e5da8f7cc75261a9173c7b6d98d4c8447fb
Sanket Supekar <sanketsupekar123@gmail.com>
2021-10-31 11:56:36 +0530

    Changed File Names

--- Practical-3/areaAndPerimeter.py
+++ Practical-3/areaAndPerimeter.py
@@ -1,3 +1,3 @@
 # 5. Write a program to calculate area and perimeter of the square
 s = int(input("Enter Side : "))
-print("Area :- ", s ** 2, "Perimeter :- ", s * 4)
+print("Area Of Square:- ", s ** 2, "Perimeter :- ", s * 4)
